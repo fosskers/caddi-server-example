dc5fb5e3727a33b71e08f96e4f01be320a2f4dbb
Colin Woodbury <colin@fosskers.ca>
2023-12-19 17:11:39 +0900

    live updates

--- src/Main.hs
+++ src/Main.hs
@@ -3,42 +3,44 @@
 
 module Main (main) where
 
-import Data.Aeson (ToJSON)
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Maybe (fromJust, listToMaybe, mapMaybe)
 import Data.Time (fromGregorian)
 import Data.Time.Calendar (Day)
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp (run)
 import Servant
+import Control.Monad.IO.Class (MonadIO(liftIO))
 
--- TODO Add an endpoint that uses a query param.
 type UserAPI = "users" :> Get '[JSON] [User]
   :<|> "users" :> Capture "name" String :> Get '[JSON] (Maybe User)
+  :<|> "users" :> "new" :> ReqBody '[JSON] Submission :> Post '[PlainText] String
+
+data Submission = Submission { subName :: !String, subAge :: !Int }
+  deriving (Eq, Show, Generic, FromJSON)
 
 data User = User {
-   name         :: !String
- , age          :: !Int
- , email        :: !String
- , registration :: !Day
+   userName  :: !String
+ , userAge   :: !Int
+ , userEmail :: !String
+ , userReg   :: !Day
 } deriving (Eq, Show, Generic, ToJSON)
 
--- TODO Add more entries.
 users :: [User]
 users =
   [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
   , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+  , User "Shunsuke Nakamura" 67 "ceo@caddi.jp"      (fromGregorian 1900 1 1)
   ]
 
 allUsers :: Handler [User]
 allUsers = pure users
 
--- TODO Demonstrate hole fits with listToMaybe.
--- TODO Demonstrate type hole with (Maybe User).
 -- TODO Refactor to make it pure.
 -- TODO Yield a better error than `null` when a match isn't found.
 getUser :: String -> Handler (Maybe User)
-getUser "" = pure Nothing
-getUser q  = pure . listToMaybe $ mapMaybe g users
+getUser "" = pure Nothing -- None
+getUser q  = pure . listToMaybe $ mapMaybe g users -- filter_map
   where
     f :: String -> Maybe String
     f n = case words n of
@@ -46,16 +48,22 @@ getUser q  = pure . listToMaybe $ mapMaybe g users
       a:_ -> Just a
 
     g :: User -> Maybe User
-    g u = case fmap (== q') . f $ name u of
+    g u = case fmap (== q') . f $ userName u of
       Just True  -> Just u
       Just False -> Nothing
       Nothing    -> Nothing
 
     q' = fromJust $ f q
 
+newSubmission :: Submission -> Handler String
+newSubmission s = do
+  liftIO $ putStrLn "Hi Ueno-san!"
+  pure $ subName s
+
 server :: Server UserAPI
 server = allUsers
   :<|> getUser
+  :<|> newSubmission
 
 api :: Proxy UserAPI
 api = Proxy
